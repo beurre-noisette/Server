0d04d7f9ced7a283f7e881bb358da89dd2165a60
sangwon <tjtkddnjs857@naver.com>
2025-03-31 00:01:14 +0900

    fix:

--- src/main/java/hello/cokezet/temporary/domain/content/repository/NoticeRepository.java
+++ src/main/java/hello/cokezet/temporary/domain/content/repository/NoticeRepository.java
@@ -2,10 +2,12 @@ package hello.cokezet.temporary.domain.content.repository;
 
 import hello.cokezet.temporary.domain.content.model.Notice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
+    @Query("SELECT n FROM Notice n ORDER BY n.isImportant DESC, n.createdAt DESC")
     List<Notice> findAllByOrderByImportantDescCreatedAtDesc();
 }
